c7c3221b1049450de8dd9b84f1975005428163c6
Jacob Boyle <41026168+MARCoTheRobot@users.noreply.github.com>
2019-08-06 11:30:52 -0400

    Added vector support and additional negators

--- languages/en/scoring-strategy.js
+++ languages/en/scoring-strategy.js
@@ -1,13 +1,17 @@
 var negators = require('./negators.json');
+var vector = require('../../lib/vector.js')
+
 
 module.exports = {
     apply: function(tokens, cursor, tokenScore) {
         if (cursor > 0) {
             var prevtoken = tokens[cursor - 1];
-            if (negators[prevtoken]) {
-                tokenScore = -tokenScore;
+            if (negators[prevtoken] || negators[prevtoken-1]) {
+                tokenScore.scalarMult(-1);
             }
         }
         return tokenScore;
     }
 };
+
+
